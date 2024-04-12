25a2c677f706d749ccbae6784777e49cdf90dd71
Patrick Wells <patrick.wells.95@gmail.com>
2024-04-12 13:48:16 -0700

    Add problem 3

---
+++ solutions/ps1/p3.py
@@ -0,0 +1,27 @@
+from charge import Charge
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    # Problem 2, dipole
+
+    x_grid, y_grid = np.meshgrid(np.arange(-25, 25), np.arange(-25, 25))
+    charges = [
+        Charge(-7.5, 0, -1),
+        Charge(7.5, 0, -1),
+        Charge(0, -7.5, 1),
+        Charge(0, 7.5, 1)
+    ]
+    potentials = np.sum(
+        [charge.potential(x_grid, y_grid) for charge in charges], axis=0)
+    plt.imshow(potentials, cmap='viridis', extent=(-25, 25, -25, 25), aspect=1)
+    plt.colorbar()
+    levels = np.linspace(-0.5, 0.5, 10)
+    plt.contour(potentials,
+                levels=levels,
+                colors='black',
+                linestyles='dotted',
+                extent=(-25, 25, -25, 25))
+    plt.show()
+    # Since we know the electric field goes like 1/r^3 for a dipole,
+    # we expect the potential to go like 1/r^2.
